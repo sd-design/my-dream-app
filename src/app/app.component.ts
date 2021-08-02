6ef37b23ed08f2e3bf9b999ad9194f0640485ccf
Alex Solovey <alex@sd-design.tk>
2021-08-02 13:13:58 +0300

    Update Angular/core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Personal my-dream-app';
+  title = 'SD Rocket my-dream-app';
 }
